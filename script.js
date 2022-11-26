4642c3ccc2b300516e98e07e0e04eb0efc6855de
thirumalesh36 <112494962+thirumalesh36@users.noreply.github.com>
2022-11-26 13:15:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+let moon = document.getElementById('moon');
+let sun = document.getElementById('white');
+let opens = document.getElementById('opens');
+let closes = document.getElementById('closes');
+let body = document.body;
+let nav = document.querySelector('nav');
+let links = document.querySelector('ul');
+let para = document.querySelector('p');
+opens.onclick=function(){
+      links.style.display='flex';
+      opens.style.display='none';
+      closes.style.display='flex';
+}
+closes.onclick=function(){
+      links.style.display='none';
+      opens.style.display='flex';
+      closes.style.display='none';
+}
+moon.onclick=function(){
+     moon.style.display='none';
+     body.style.background='black';
+     sun.style.display='block'
+      
+}
+sun.onclick=function(){
+     moon.style.display='block';
+     body.style.background='white';
+     sun.style.display='none';
+    
+}
